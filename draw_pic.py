ba0ae0861f4bd55e53ab15f018e88c92d4724ca6
shubin ma <1369830844@qq.com>
2025-07-15 21:51:29 +0800

    Delete draw_pic.py

--- draw_pic.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-def draw_grey(matrix,width,height):
-    image=Image.new('L',(width,height))
-    # print(width,height)
-    for i in range(width):
-        for j in range(height):
-            value=matrix[i][j]*255
-            image.putpixel((j,i),int(value))
-    # image.save("grep_image.png")
